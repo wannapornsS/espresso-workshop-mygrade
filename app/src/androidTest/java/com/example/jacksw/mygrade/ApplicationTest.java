faf83a6f643abaca19b86590c5979b7aa18f9600
Prayoch Rujira <prayoch@clockupstudio.com>
2019-12-18 23:21:38 +0700

    Fixed Sample test file

--- app/src/androidTest/java/com/example/jacksw/mygrade/ApplicationTest.java
+++ app/src/androidTest/java/com/example/jacksw/mygrade/ApplicationTest.java
@@ -1,13 +1,14 @@
 package com.example.jacksw.mygrade;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.runner.AndroidJUnit4;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest{
+
+    @Test
+    public void testCaseExample() {
     }
 }
